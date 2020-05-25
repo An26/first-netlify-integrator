3f98c22f828d1a2c7d8dbce6b75e776cad6024fc
An Huynh <anhuynh26@gmail.com>
2020-05-25 18:56:27 -0500

    added something

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          An's a muthfuckin badassss
         </a>
       </header>
     </div>
